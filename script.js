81a4346a1a1baaff3282e67710dd32c5a1c55215
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-24 23:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ function countLetters() {
   const textBox = document.getElementById("evaluatedText");
   const letterCount = document.getElementById("letterCount");
   const textLength = textBox.value.length;
-	if(textLength==0)
-		letterCount.innerText = NaN;
-	else
-		letterCount.innerText = textLength;
-  
+  letterCount.innerText = textLength;
 }
